40c1ed58ca982af62528dd47c7d104f91e7c897d
Achille AROUKO <achille.arouko@gmail.com>
2016-12-23 16:09:00 +0100

    utils

---
+++ modele/src/main/java/modele/utils/Coordinates.java
@@ -0,0 +1,14 @@
+package modele.utils;
+
+/**
+ * Created by lekaizer on 23/12/2016.
+ */
+public class Coordinates {
+    public int x;
+    public int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
